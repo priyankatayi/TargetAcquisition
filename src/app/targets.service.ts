7fbe648e494bb4196731db557fc10ecb76efa0d3
pinkytayi <pinkytayi@gmail.com>
2019-08-09 17:11:07 -0400

    updated the comments for service file

--- src/app/targets.service.ts
+++ src/app/targets.service.ts
@@ -5,7 +5,6 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { HttpHeaders } from '@angular/common/http';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -14,15 +13,17 @@ export class TargetsService {
   constructor(private httpClient: HttpClient) { }
   baseUrl = 'http://localhost:3000/companies';
 
+  // to READ targets list data from server
   getListOfTargets(): Observable<Target[]> {
     return this.httpClient.get<Target[]>(this.baseUrl).pipe(catchError(this.handleError));
   }
 
+  // to get particular target data for modifying
   getSingleTarget(id: number): Observable<Target> {
     return this.httpClient.get<Target>(`${this.baseUrl}/${id}`)
       .pipe(catchError(this.handleError));
   }
-
+  // to WRITE the newly created target data into json file
   addTarget(data: Target): Observable<Target> {
     return this.httpClient.post<Target>(this.baseUrl, data, {
       headers: new HttpHeaders({
@@ -31,6 +32,7 @@ export class TargetsService {
     }).pipe(catchError(this.handleError));
   }
 
+  // to UPDATE the modified information of exisiting target
   updateTarget(data: Target): Observable<Target> {
     return this.httpClient.put<Target>(`${this.baseUrl}/${data.id}`, data, {
       headers: new HttpHeaders({
@@ -39,6 +41,7 @@ export class TargetsService {
     }).pipe(catchError(this.handleError));
   }
 
+  // to DELETE the target data from json
   deleteTarget(id: number): Observable<void> {
     console.log(id);
     return this.httpClient.delete<void>(`${this.baseUrl}/${id}`)
